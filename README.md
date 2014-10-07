6deb860188dc70562ca10f3cbbba54451fcdb719
Carl Craig <carlcraig@3c-studios.com>
2014-10-07 17:27:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+Grunt App Icons
+===============
+
+Installation
+------------
+
+```sh
+npm install grunt-app-icons --save-dev
+```
+
+
+
+Example Configuration
+---------------------
+
+```js
+appIcons: {
+  all: {
+    src: 'src/icon/app-icon.png',
+    dest: 'src/icon/',
+    options: {
+      createDirectories: true,
+      type: ['all']
+    }
+  },
+  www: {
+    src: 'src/icon/app-icon.png',
+    dest: 'www/',
+    options: {
+      type: ['touch', 'favicon'] // android, ios
+    }
+  }
+}
+```
+
+Html
+----
+
+```html
+<link rel="icon" sizes="192x192" href="touch-icon-192x192.png">
+<link rel="apple-touch-icon-precomposed" sizes="180x180" href="apple-touch-icon-180x180-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+```
+
+License
+=======
+
+grunt-app-icons is licensed with the MIT license.
+
+See LICENSE-MIT for more details.
---
+++ tasks/tc-app-icons.js
@@ -0,0 +1,164 @@
+module.exports = function ( grunt ) {
+  'use strict';
+
+  var fs = require( 'fs' );
+  var path = require( 'path' );
+
+  grunt.registerMultiTask( 'appIcons', 'create app icons', function () {
+
+    var done = this.async();
+    var im = require( 'imagemagick' );
+
+    var options = this.options( {
+      type: [], // all, favicon, touchicon, ios, android
+      createDirectories: false,
+      precomposed: true,
+      precomposedString: '-precomposed'
+    } );
+
+    var remaining = this.files.length;
+
+    this.files.forEach( function ( file ) {
+
+      if ( options.type.length > 0 ) {
+
+        var image = file.src[0];
+        var folder = path.dirname( file.dest ) + '/' + path.basename( file.dest ) + '/';
+        var folders = [folder];
+
+        if ( options.type.indexOf( 'all' ) >= 0 ) {
+          options.type = ['favicon', 'touch', 'ios', 'android']
+        }
+
+        if ( options.createDirectories ) {
+          folders = [];
+          options.type.forEach( function ( type ) {
+            folders.push( folder + type + '/' );
+          } );
+        }
+
+        var remainingTypes = options.type.length;
+        var remainingFolders = folders.length;
+
+        folders.forEach( function ( f ) {
+          fs.mkdirParent( f, null, function () {
+            if ( -- remainingFolders <= 0 ) {
+              options.type.forEach( function ( type ) {
+                if ( icons.hasOwnProperty( type ) ) {
+                  var remainingIcons = icons[type].length;
+                  icons[type].forEach( function ( iconOptions ) {
+                    var iconFolder = folder;
+                    if (options.createDirectories) {
+                      iconFolder += type + '/';
+                    }
+                    var iconName = iconOptions.name;
+                    if (options.precomposed) {
+                      iconName = iconName.replace(/%precomposed%/g, options.precomposedString);
+                    }
+                    var args = [image].concat( iconOptions.args ).concat( [iconFolder + iconName] );
+                    im.convert( args, function ( error, stdout ) {
+                      if (error) {
+                        grunt.log.errorlns(error);
+                      } else {
+                        grunt.log.writeln('Created: ' + iconName);
+                      }
+                      if ( --remainingIcons <= 0) {
+                        if ( -- remainingTypes <= 0 ) {
+                          if ( -- remaining <= 0 ) {
+                            done();
+                          }
+                        }
+                      }
+                    } );
+                  } );
+                } else {
+                  if ( -- remainingTypes <= 0 ) {
+                    if ( -- remaining <= 0 ) {
+                      done();
+                    }
+                  }
+                }
+              } );
+            }
+          } );
+        } );
+
+      } else {
+        grunt.log.error( 'No type option specified, allowed: all, favicon, touch, ico, ios, android' );
+        if ( -- remaining <= 0 ) {
+          done();
+        }
+      }
+
+    } );
+
+  } );
+
+  var icons = {
+    ios: [
+      {name: 'icon-60.png', args: ['-resize', '60x60']},
+      {name: 'icon-60@2x.png', args: ['-resize', '120x120']},
+      {name: 'icon-76.png', args: ['-resize', '76x76']},
+      {name: 'icon-76@2x.png', args: ['-resize', '152x152']},
+      {name: 'icon-40.png', args: ['-resize', '40x40']},
+      {name: 'icon-40@2x.png', args: ['-resize', '80x80']},
+      {name: 'icon.png', args: ['-resize', '57x57']},
+      {name: 'icon@2x.png', args: ['-resize', '114x114']},
+      {name: 'icon-72.png', args: ['-resize', '72x72']},
+      {name: 'icon-72@2x.png', args: ['-resize', '144x144']},
+      {name: 'icon-small.png', args: ['-resize', '29x29']},
+      {name: 'icon-small@2x.png', args: ['-resize', '58x58']},
+      {name: 'icon-50.png', args: ['-resize', '50x50']},
+      {name: 'icon-50@2x.png', args: ['-resize', '100x100']}
+    ],
+    android: [
+      {name: 'icon-ldpi.png', args: ['-resize', '36x36']},
+      {name: 'icon-mdpi.png', args: ['-resize', '48x48']},
+      {name: 'icon-hdpi.png', args: ['-resize', '72x72']},
+      {name: 'icon-xhdpi.png', args: ['-resize', '96x96']},
+      {name: 'icon-xxhdpi.png', args: ['-resize', '144x144']},
+      {name: 'icon-xxxhdpi.png', args: ['-resize', '192x192']}
+    ],
+    favicon: [
+      {
+        name: 'favicon.ico',
+        args: [
+          '-bordercolor', 'white',
+          '-border', '0',
+          '(', '-clone', '0', '-resize', '16x16', ')',
+          '(', '-clone', '0', '-resize', '32x32', ')',
+          '(', '-clone', '0', '-resize', '48x48', ')',
+          '(', '-clone', '0', '-resize', '64x64', ')',
+          '-delete', '0',
+          '-alpha', 'off',
+          '-colors', '256'
+        ]
+      }
+    ],
+    touch: [
+      {name: 'apple-touch-icon%precomposed%.png', args: ['-resize', '57x57']},
+      {name: 'apple-touch-icon-76x76%precomposed%.png', args: ['-resize', '76x76']},
+      {name: 'apple-touch-icon-120x120%precomposed%.png', args: ['-resize', '120x120']},
+      {name: 'apple-touch-icon-152x152%precomposed%.png', args: ['-resize', '152x152']},
+      {name: 'apple-touch-icon-180x180%precomposed%.png', args: ['-resize', '180x180']},
+      {name: 'touch-icon-128x128.png', args: ['-resize', '128x128']},
+      {name: 'touch-icon-192x192.png', args: ['-resize', '192x192']}
+    ]
+  };
+
+  fs.mkdirParent = function ( dirPath, mode, callback ) {
+    //Call the standard fs.mkdir
+    fs.mkdir( dirPath, mode, function ( error ) {
+      //When it fail in this way, do the custom steps
+      if ( error && error.errno === 34 ) {
+        //Create all the parents recursively
+        fs.mkdirParent( path.dirname( dirPath ), mode, callback );
+        //And then the directory
+        fs.mkdirParent( dirPath, mode, callback );
+      }
+      //Manually run the callback since we used our own callback to do all these
+      callback && callback( error );
+    } );
+  };
+
+};
